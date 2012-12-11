1cf664cc741271f32a5e4ead6931373c5afaf1b1
Jignesh Gohel <jignesh.ghl@gmail.com>
2012-12-11 20:41:16 +0530

    Empty skeleton for project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shareit::Application.config.secret_token = 'f0128f7cc3d18a24fbc62b12bc41827536f1c6d3de3ae21f12f1a25ce8be65642dfe185ae30abeb04caaf427abc189f05a00d863d11e85edf6b5610577133a47'
